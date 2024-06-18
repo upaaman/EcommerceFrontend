084c81ef5798b2f387e1510ce57bed30cd23c86a
upaaman <upadhyayaman9685@gmail.com>
2024-06-18 18:21:59 +0530

    first commit

---
+++ src/customer/Components/adreess/AdreessCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const AddressCard = ({address}) => {
+  console.log("this is address",address)
+  return (
+    <div>
+      {/* <h1 className="text-lg font-semibold py-4">Delivery Adress</h1> */}
+      <div className="space-y-3">
+        <p className="font-semibold">{`${address?.firstName} ${address?.lastName}`}</p>
+
+        <p>
+          {`${address?.streetAddress} ${address?.city} ${address?.state} ${address?.zipCode}`}
+        </p>
+
+        <div className="space-y-1">
+          <p className="font-semibold">Phone Number</p>
+          <p>{address?.mobile}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddressCard;
